1ec6cd10793065a3a733ad5a634ce86166fcffc4
hhyowon <hyowon0605@gmail.com>
2023-06-20 12:32:41 +0900

    dd

---
+++ src/main/java/com/example/study_servlets/controlls/GetRequestParamsServlet.java
@@ -0,0 +1,44 @@
+package com.example.study_servlets.controlls;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns ="/getRequsetParamsServlet")
+public class GetRequestParamsServlet extends HttpServlet{
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    try {
+        // PrintWriter printWriter = response.getWriter();
+        // String contents ="";
+        // printWriter.println(contents);
+        // printWriter.close();
+            String firstName = "yebinhyowon";
+            String secondName = "kimcho";
+            PrintWriter printWriter = response.getWriter();
+            // String contents = "yebin kim !";
+            String contents = "<!DOCTYPE html>\r\n" + //
+                    "<html lang=\"en\">\r\n" + //
+                    "<head>\r\n" + //
+                    "    <meta charset=\"UTF-8\">\r\n" + //
+                    "    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\r\n" + //
+                    "    <title>Document</title>\r\n" + //
+                    "</head>\r\n" + //
+                    "<body>\r\n" + //
+                    "    <div> first Name : "+firstName+" </div>\r\n" + //
+                    "    <div> second Name : "+secondName+" </div>\r\n" + //
+                    "</body>\r\n" + //
+                    "</html>";
+            printWriter.println(contents);
+            printWriter.close();
+    } catch (Exception e) {
+       System.out.println(e.getMessage()); // TODO: handle exception
+    }
+        
+    }
+}
