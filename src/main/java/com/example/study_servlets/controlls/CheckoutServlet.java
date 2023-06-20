19600e9eaa7d120871bd191268d8079f163a2177
hhyowon <hyowon0605@gmail.com>
2023-06-20 18:13:41 +0900

    효

---
+++ src/main/java/com/example/study_servlets/controlls/CheckoutServlet.java
@@ -0,0 +1,11 @@
+package com.example.study_servlets.controlls;
+// contorlls/Checkout*Servlet.java
+// - form : CheckoutFormServlet.java
+// + refer : https://getbootstrap.com/docs/5.3/examples/checkout/
+// - CheckoutResponseServlet.java
+// + 항목 맞게 표시
+// - github와 동작 url 공유
+
+public class CheckoutServlet {
+    
+}
