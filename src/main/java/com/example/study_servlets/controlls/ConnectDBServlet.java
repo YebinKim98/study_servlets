2c193096bfa4ad70282f3dbb8186956ad48b7e9e
hhyowon <hyowon0605@gmail.com>
2023-06-21 18:35:53 +0900

    gydnjs

--- src/main/java/com/example/study_servlets/controlls/ConnectDBServlet.java
+++ src/main/java/com/example/study_servlets/controlls/ConnectDBServlet.java
@@ -6,6 +6,8 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -13,20 +15,17 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.example.study_servlets.controlls.commons.Commons;
+import com.example.study_servlets.daos.CarlnforsDao;
+import com.example.study_servlets.daos.FactorysDao;
+
 @WebServlet(urlPatterns = "/connectDBServlet") 
 public class ConnectDBServlet extends HttpServlet {
     @Override // 상속이 된 걸 확장하는구나!! 알 수 있음
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         try {
-            // - MySQL workbench 실행 : JDBC
-            // - User/password와 접속 IP:port 접속
-            String url = "jdbc:mysql://192.168.0.43:3306/db_cars";
-            String user = "yojulab";
-            String password = "!yojulab*";
 
-            Connection connection = DriverManager.getConnection(url, user, password);
-            System.out.println("DB연결 성공\n");
             // 클라이언트에 html화면제공
             String contents = "<!DOCTYPE html>\r\n" + //
                     "<html lang=\"en\">\r\n" + //
@@ -49,64 +48,84 @@ public class ConnectDBServlet extends HttpServlet {
                     "            </thead>\r\n" + //
                     "            <tbody>\r\n";
 
-            // - query Edit
-            Statement statement = connection.createStatement();
-            String query = "SELECT * FROM factorys";
-            ResultSet resultSet = statement.executeQuery(query); // 하면 결과값이 return됨. ..??
-            while (resultSet.next()) {
-                // query의 table을 가져올건데, 나올 값이 끝날때까지 돌기. next는 자체내에서 뭉치를 던지는 동작을함. 레코드가 다음레코드로
-                // 넘어감.
+            // - query Edit   
+                        CarlnforsDao  carlnforsDao  = new CarlnforsDao();
+            ArrayList carlnforsList = new ArrayList();
+            carlnforsList = carlnforsDao.selectAll();
+
+            for(int i =0; i<  carlnforsList.size(); i++)
+            {
+                HashMap hashMap = new HashMap();
+                hashMap = (HashMap)  carlnforsList.get(i);
                 contents = contents + "  <tr>\r\n" + //
-                        "                    <td>" + resultSet.getString("COMPANY_ID") + "</td>\r\n" + //
-                        "                    <td>" + resultSet.getString("COMPANY") + "</td>\r\n" + //
+                        "                    <td>" + hashMap.get("CAR_NAME") + "</td>\r\n" + //
+                        "                    <td>" + hashMap.get("YEAR") + "</td>\r\n" + //
+                        "                     <td>" + hashMap.get("CAR_INFOR_ID") + "</td>\r\n" + //
+                        "                    <td>" + hashMap.get("COMPANY_ID") + "</td>\r\n" + //
                         "                </tr>\r\n";
             }
+
+            // //FactorysDao 메소드 
+            // FactorysDao factorysDao = new FactorysDao();
+            // ArrayList factoryList = new ArrayList();
+            // factoryList = factorysDao.selectAll();
+
+            // for(int i =0; i< factoryList.size(); i++)
+            // {
+            //     HashMap hashMap = new HashMap();
+            //     hashMap = (HashMap) factoryList.get(i);
+            //     contents = contents + "  <tr>\r\n" + //
+            //             "                    <td>" + hashMap.get("COMPANY_ID") + "</td>\r\n" + //
+            //             "                    <td>" + hashMap.get("COMPANY") + "</td>\r\n" + //
+            //             "                </tr>\r\n";
+            // }
 
             contents = contents + "</tbody>\r\n" + //
                     "</table>\r\n" + //
                     "</div>\r\n" + //
                     "</body>\r\n" + //
                     "</html>";
+                    
             // 클라이언트에 html화면제공
             response.setContentType("text/html;charset=UTF-8");
             PrintWriter printWriter = response.getWriter();
             printWriter.println(contents);
             printWriter.close();
 
-            // SELECT count(*) AS CNT FROM factorys;
-            query = "SELECT count(*) AS CNT FROM factorys";
-            resultSet = statement.executeQuery(query);
-            int totalcount = 0;
-            while (resultSet.next()) {
-                System.out.println(resultSet.getInt("CNT"));
-                totalcount = resultSet.getInt("CNT");
-            }
+            // // SELECT count(*) AS CNT FROM factorys;
+            // query = "SELECT count(*) AS CNT FROM factorys";
+            // resultSet = statement.executeQuery(query);
+            // int totalcount = 0;
+            // while (resultSet.next()) {
+            //     System.out.println(resultSet.getInt("CNT"));
+            //     totalcount = resultSet.getInt("CNT");
+            // }
             // INSERT INTO factorys
             // (COMPANY_ID,COMPANY)
             // value
             // ('CAR-01', 'AUDI');
 
-            String COMPANY_ID = "CAR-01";
-            String COMPANY = "AUDI";
-            // query ="INSERT INTO factorys " + "(COMPANY_ID,COMPANY) " + "value " +
-            // "('"+COMPANY_ID+"', '"+COMPANY+"') ";
-            // // 변수를 넣어줄때 싱글 쿼테이션 '' 안에 더블 쿼테이션 ""을 넣고 ++사이에 변수를 넣어줘야한다.
+            // String COMPANY_ID = "CAR-01";
+            // String COMPANY = "AUDI";
+            // // query ="INSERT INTO factorys " + "(COMPANY_ID,COMPANY) " + "value " +
+            // // "('"+COMPANY_ID+"', '"+COMPANY+"') ";
+            // // // 변수를 넣어줄때 싱글 쿼테이션 '' 안에 더블 쿼테이션 ""을 넣고 ++사이에 변수를 넣어줘야한다.
+            // // int count = statement.executeUpdate(query);
+
+            // COMPANY_ID = "CAR-01";
+            // COMPANY = "페라리";
+            // query = "UPDATE factorys " + "SET COMPANY ='" + COMPANY + "' " + "WHERE COMPANY_ID ='" + COMPANY_ID + "' "; // CAR-01
+            //                                                                                                             // AUDI를
+            //                                                                                                             // 페라리로
+            //                                                                                                             // 업데이트
+
+            // // DELETE FROM factorys
+            // // WHERE COMPANY_ID = 'CAR-01';
+            // query = "DELETE FROM factorys " + "WHERE COMPANY_ID = '" + COMPANY_ID + "'"; // CAR-01 페라리 삭제
+
             // int count = statement.executeUpdate(query);
 
-            COMPANY_ID = "CAR-01";
-            COMPANY = "페라리";
-            query = "UPDATE factorys " + "SET COMPANY ='" + COMPANY + "' " + "WHERE COMPANY_ID ='" + COMPANY_ID + "' "; // CAR-01
-                                                                                                                        // AUDI를
-                                                                                                                        // 페라리로
-                                                                                                                        // 업데이트
-
-            // DELETE FROM factorys
-            // WHERE COMPANY_ID = 'CAR-01';
-            query = "DELETE FROM factorys " + "WHERE COMPANY_ID = '" + COMPANY_ID + "'"; // CAR-01 페라리 삭제
-
-            int count = statement.executeUpdate(query);
-
-            System.out.println();
+            // System.out.println();
 
         } catch (Exception e) {
             System.out.println(e.getMessage()); // 에러 내용을 볼수 있는 명령문
